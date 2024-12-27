f84691f72fdd4dd0a95154d2b73e4461d2a09ad5
pixel-echo <tara.chakkithara@icloud.com>
2024-12-26 23:38:48 -0800

    fixed bug in align.py. calculated alignment score correctly.

--- align.py
+++ align.py
@@ -1,10 +1,16 @@
-from pandas import read_csv
+import pandas as pd
 
-male_edges = {(r[0], r[1]): int(r[2]) for r in read_csv("data/male.gz")[1:]}
-female_edges = {(r[0], r[1]): int(r[2]) for r in read_csv("data/female.gz")[1:]}
-matching = {r[0]: r[1] for r in read_csv("data/benchmark.gz")[1:]}
+male = pd.read_csv("data/male.gz")
+female = pd.read_csv("data/female.gz")
+mapping = pd.read_csv("data/benchmark.gz")
+
+domain = {tuple(row[:2]) : row[2] for row in male.values}
+codomain = {tuple(row[:2]) : row[2] for row in female.values}
+
+matching = {r[0]: r[1] for r in mapping.values}
 alignment = 0
-for male_nodes, edge_weight in male_edges.items():
+
+for male_nodes, edge_weight in domain.items():
   female_nodes = (matching[male_nodes[0]], matching[male_nodes[1]])
-  alignment += min(edge_weight, female_edges.get(female_nodes, 0))
+  alignment += min(edge_weight, codomain.get(female_nodes, 0))
 print(f"{alignment=}")
